94d168a983b37c5397e2360ac65a67a190b6429a
BigMurloc <s21159@pjwstk.edu.pl>
2023-01-28 15:47:41 +0100

    init

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GoogleTest {
+
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setUpDriver(){
+        System.setProperty("webdriver.gecko.driver", "resources/geckodriver.exe");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addPreference("intl.accept_languages", "en-US");
+        driver = new FirefoxDriver(firefoxOptions);
+
+        // Implicity wait -> max czas na znalezienie elementu na stronie
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        driver.get("https://duckduckgo.com/");
+    }
+
+    @AfterAll
+    public static void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void testTitlePage() {
+        assertTrue(driver.getTitle().contains("DuckDuckGo"));
+    }
+
+    @Test
+    public void testSource(){
+        String source = driver.getPageSource();
+        assertTrue(source.contains("DuckDuckGo"));
+    }
+
+}
